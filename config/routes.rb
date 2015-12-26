8025003c42499550a78e025300a3a3fb58563db6
Federico Bucchi <b.federico@gmail.com>
2015-12-26 10:40:20 -0800

    add v1 version

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
-    # We are going to list our resources here
+    scope module: :v1 do
+      # We are going to list our resources here
+    end
   end
 end
