793245e37939c9032915647d986666a344c94915
Federico Bucchi <b.federico@gmail.com>
2015-12-26 23:15:09 -0800

    add crud actions for members

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -1,7 +1,51 @@
 class Api::V1::MembersController < Api::V1::ApplicationController
-  def show
-    member = Member.find(params[:id])
+  before_action :set_member, only: [:show, :update, :destroy]
 
-    render(json: member.to_json)
+  # GET /members
+  def index
+    members = Member.all()
+
+    render(json: members.to_json)
   end
+
+  # POST /members
+  def create
+    member = Member.new(member_params)
+
+    if member.save
+      render(json: member.to_json)
+    else
+      render(json: member.errors.to_json)
+    end
+  end
+
+  # GET /members/:id
+  def show
+    render(json: @member.to_json)
+  end
+
+  # PUT /members/:id
+  def update
+    if @member.update(member_params)
+      render(json: @member.to_json)
+    else
+      render(json: @member.errors.to_json)
+    end
+  end
+
+  # DELETE /member/:id
+  def destroy
+    if @member.destroy
+      render(json: @member.to_json)
+    end
+  end
+
+  private
+    def set_member
+      @member = Member.find(params[:id])
+    end
+
+    def member_params
+      params.require(:member).permit(:name, :lastname, :email)
+    end
 end
